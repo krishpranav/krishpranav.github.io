b385edb1095fcbe6782f26f921ab3f06b29864ec
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-11-19 17:05:46 +0530

    added date on when website would be live

--- app/page.tsx
+++ app/page.tsx
@@ -14,14 +14,15 @@ import { FloatingNav } from "@/components/ui/FloatingNavbar";
 const Home = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl w-full">
+      {/* <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
         <Experience />
         <Approach />
         <Footer />
-      </div>
+      </div> */}
+      <p>This website will be live by April 2025. please be patient.</p>
     </main>
   );
 };
