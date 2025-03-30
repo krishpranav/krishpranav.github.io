417ddb1db51446e7490a801c29e2a6ae68a6f318
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-30 17:22:21 +0530

    next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const WindiCSS = require('windicss-webpack-plugin');
 const { withAxiom } = require('next-axiom');
 
+const isGithubPages = process.env.GITHUB_ACTIONS || false;
+const repoName = 'your-repo-name'; // Change this to your GitHub repo name
+
 const ContentSecurityPolicy = `
   child-src *.google.com streamable.com;
   connect-src *;
@@ -17,48 +20,40 @@ const ContentSecurityPolicy = `
  * @type {import('next').NextConfig}
  */
 const config = {
+	output: 'export', // Needed for static site deployment
+	assetPrefix: isGithubPages ? `/${repoName}/` : '',
+	basePath: isGithubPages ? `/${repoName}` : '',
+	trailingSlash: true, // Ensures all paths end with a slash for static hosting
+
 	images: {
 		domains: [
-			// Discord assets
 			'cdn.discordapp.com',
-
-			// GitHub assets
 			'raw.githubusercontent.com',
-
-			// Spotify Album Art
 			'i.scdn.co',
-
-			// Streamable thumbnails
 			'cdn-cf-east.streamable.com',
-
-			// Unsplash
 			'source.unsplash.com',
 			'images.unsplash.com',
 		],
+		unoptimized: true, // GitHub Pages doesn't support Next.js image optimization
 	},
-	// Inspired by: https://github.com/leerob/leerob.io/blob/main/next.config.js#L44-L81
+
 	async headers() {
 		return [
 			{
 				source: '/(.*)',
 				headers: [
-					// https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
 					{
 						key: 'Content-Security-Policy',
 						value: ContentSecurityPolicy.replace(/\n/g, ''),
 					},
-					// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
 					{
 						key: 'Referrer-Policy',
 						value: 'origin-when-cross-origin',
 					},
-					// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
 					{
 						key: 'Strict-Transport-Security',
 						value: 'max-age=31536000; includeSubDomains; preload',
 					},
-					// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
-					// Opt-out of Google FLoC: https://amifloced.org/
 					{
 						key: 'Permissions-Policy',
 						value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()',
@@ -67,19 +62,11 @@ const config = {
 			},
 		];
 	},
+
 	reactStrictMode: true,
 	swcMinify: true,
-	webpack: (config, { dev, isServer }) => {
-		// TODO: Temp disabled as since upgrading `next` to v12.2.3 production builds fail & this seems to be the cause
-		// Replace React with Preact only in client production build
-		// if (!dev && !isServer) {
-		// 	Object.assign(config.resolve.alias, {
-		// 		react: 'preact/compat',
-		// 		'react-dom/test-utils': 'preact/test-utils',
-		// 		'react-dom': 'preact/compat',
-		// 	});
-		// }
 
+	webpack: (config, { dev, isServer }) => {
 		config.plugins.push(new WindiCSS());
 
 		config.module.rules.push({
